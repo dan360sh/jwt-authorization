2b14aa30d6908a7dfb90fead367232cfe2feb304
dan <swqazxcd@gmail.com>
2023-07-29 21:00:18 +0300

    delete

--- libs/jwt-authorization/src/exception/unauthorized.exception.ts
+++
@@ -1,7 +0,0 @@
-import { UnauthorizedException } from '@nestjs/common';
-
-export class UnauthorizedAccess extends UnauthorizedException {
-    constructor() {
-        super('Unauthorized access', 'Unauthorized');
-    }
-}
